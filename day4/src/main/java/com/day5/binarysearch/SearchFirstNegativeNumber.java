ca556ce8e56ab09e7dc18ed57b50278d4a6ca406
sarowargupta <sarowargupta2001@gmail.com>
2025-02-05 18:39:16 +0530

    [Sarowar Gupta]add LinearSearch Question1

---
+++ day4/src/main/java/com/day5/binarysearch/SearchFirstNegativeNumber.java
@@ -0,0 +1,34 @@
+package com.day5.binarysearch;
+
+public class SearchFirstNegativeNumber {
+
+           //method to find first negative number
+        public static int findFirstNegative(int[] arr) {
+            //iterate loop through length of the array
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < 0) {
+
+                    // Return the index of the first negative number
+                    return i;
+                }
+            }
+            // Return -1 if no negative number is found
+            return -1;
+        }
+
+        public static void main(String[] args) {
+            //Initialize array with numbers with positive and negative
+            int[] numbers = {1, -5, -7, 7, 8, -6};
+
+            //call the method check number is negative and return index
+            int index = findFirstNegative(numbers);
+
+            //check first negative number found or not
+            if (index != -1) {
+                System.out.println("First negative number found at index: " + index);
+            } else {
+                System.out.println("No negative number found in the array.");
+            }
+        }
+
+}
---
+++ day4/src/test/java/SearchFirstNegativeNumberTest.java
@@ -0,0 +1,14 @@
+import com.day5.binarysearch.SearchFirstNegativeNumber;
+import org.junit.jupiter.api.Test;
+import static java.lang.System.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SearchFirstNegativeNumberTest {
+    @Test
+    void search(){
+        SearchFirstNegativeNumber obj = new SearchFirstNegativeNumber();
+        int[] numbers = {1, -5, -7, 7, 8, -6};
+        assertEquals(1,obj.findFirstNegative(numbers));
+        out.println("First negative number found at index: " + obj.findFirstNegative(numbers));
+    }
+}
